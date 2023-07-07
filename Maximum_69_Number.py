078a33cba84f4e1c426ac90b3176367db04758cb
psriabhikoushik <114810058+psriabhikoushik@users.noreply.github.com>
2023-07-07 11:21:36 +0530

    n=input() l=list(n) for i in range(len(l)):     if l[i]=='6':         l[i]='9'         break for i in range(len(l)):     print(l[i],end='')

--- Maximum_69_Number.py
+++ Maximum_69_Number.py
@@ -1,16 +1,8 @@
-n=int(input())
-s=0
-while n:
-    s*=10
-    s+=(n%10)
-    n//=10
-q=0
-while s:
-    n*=10
-    if s%10==6 and q==0:
-        n+=9
-        q=1
-    else:
-        n+=s%10
-    s//=10    
-print(n)        
+n=input()
+l=list(n)
+for i in range(len(l)):
+    if l[i]=='6':
+        l[i]='9'
+        break
+for i in range(len(l)):
+    print(l[i],end='')
