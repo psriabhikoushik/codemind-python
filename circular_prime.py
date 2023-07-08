d38148c53ea10619912a6934dd0d3d4c64d31ea4
psriabhikoushik <114810058+psriabhikoushik@users.noreply.github.com>
2023-07-08 10:36:22 +0530

    def pr(n):     if n==1:         return False     for i in range(2,int(n**0.5)+1):         if n%i==0:             return False     return True n=int(input()) l=str(n) s=l[::-1] s=int(s) if pr(n)==True and pr(s)==True:     print('circular prime') elif pr(n)==True:     print('prime but not a circular prime') else:     print('not prime')

--- circular_prime.py
+++ circular_prime.py
@@ -1,4 +1,4 @@
-def prime(n):
+def pr(n):
     if n==1:
         return False
     for i in range(2,int(n**0.5)+1):
@@ -9,9 +9,9 @@ n=int(input())
 l=str(n)
 s=l[::-1]
 s=int(s)
-if prime(n)==True and prime(s)==True:
+if pr(n)==True and pr(s)==True:
     print('circular prime')
-elif prime(n)==True:
+elif pr(n)==True:
     print('prime but not a circular prime')
 else:
     print('not prime')
