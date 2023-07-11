656e2ea62e0d075a22368a47a60fec83ce283a05
psriabhikoushik <114810058+psriabhikoushik@users.noreply.github.com>
2023-07-11 10:18:24 +0530

    n=int(input()) f=n r=0 while n>0:     s=n%10     r=r*10+s     n=n//10 if r==f:     print(True) else:     print(False)

--- Palindrome.py
+++ Palindrome.py
@@ -1,11 +1,11 @@
 n=int(input())
+f=n
 r=0
-w=n
 while n>0:
-    r1=n%10
-    r=r*10+r1
-    n//=10
-if w==r:
+    s=n%10
+    r=r*10+s
+    n=n//10
+if r==f:
     print(True)
 else:
     print(False)
