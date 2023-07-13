0ef9546a92f616bac6a33593d4b53a1d2ca2bc32
psriabhikoushik <114810058+psriabhikoushik@users.noreply.github.com>
2023-07-13 12:01:28 +0530

    n=int(input()) a=list(map(int,input().split())) k=[] k1=[] g=0 for i in range(len(a)):     if a[i]%2==0:         k.append(a[i])     else:         k1.append(a[i]) c=(k1+k) print(*c)

---
+++ Array_of_odd_and_even_elements.py
@@ -0,0 +1,12 @@
+n=int(input())
+a=list(map(int,input().split()))
+k=[]
+k1=[]
+g=0
+for i in range(len(a)):
+    if a[i]%2==0:
+        k.append(a[i])
+    else:
+        k1.append(a[i])
+c=(k1+k)
+print(*c)
